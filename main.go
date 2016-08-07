5905e187d1e64b6157315b9adbbcfbd5abaaa50b
Kostas Christidis <kostas@christidis.io>
2016-08-07 17:41:20 -0400

    Add config options to simple producer

--- main.go
+++ main.go
@@ -2,32 +2,58 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/Shopify/sarama"
 )
 
+var (
+	brokerAddr  = "localhost:9092"
+	prodID      = "prod1"
+	topicName   = "test"
+	messageText = "hello world"
+
+	concurrentReqs = 1
+	requiredAcks   = sarama.WaitForAll
+
+	version = sarama.V0_9_0_1
+)
+
 func main() {
-	brokerAddr := "localhost:9092"
-	producer, err := sarama.NewSyncProducer([]string{brokerAddr}, newConfig())
-	if err != nil {
-		log.Fatalln(err)
-	}
+	producer := newProducer(brokerAddr, newConfig(concurrentReqs, requiredAcks, prodID, version))
 	defer func() {
 		if err := producer.Close(); err != nil {
 			log.Fatalln(err)
 		}
 	}()
+	for {
+		sendMessage(producer, topicName, messageText)
+		time.Sleep(1 * time.Second)
+	}
+}
 
-	msg := &sarama.ProducerMessage{Topic: "test", Value: sarama.StringEncoder("hello there!")}
-	partition, offset, err := producer.SendMessage(msg)
+func newConfig(concurrentReqs int, requiredAcks sarama.RequiredAcks, prodID string, version sarama.KafkaVersion) *sarama.Config {
+	conf := sarama.NewConfig()
+	conf.Net.MaxOpenRequests = concurrentReqs
+	conf.Producer.RequiredAcks = requiredAcks
+	conf.ClientID = prodID
+	conf.Version = version
+	return conf
+}
+
+func newProducer(brokerAddr string, config *sarama.Config) sarama.SyncProducer {
+	producer, err := sarama.NewSyncProducer([]string{brokerAddr}, config)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return producer
+}
+
+func sendMessage(producer sarama.SyncProducer, topic string, message string) {
+	newMessage := &sarama.ProducerMessage{Topic: topic, Value: sarama.StringEncoder(message)}
+	partition, offset, err := producer.SendMessage(newMessage)
 	if err != nil {
 		log.Printf("Failed to send message: %s\n", err)
 	}
 	log.Printf("Message sent to partition %d at offset %d\n", partition, offset)
-}
-
-func newConfig() *sarama.Config {
-	conf := sarama.NewConfig()
-	conf.ClientID = "prod1"
-	return conf
 }
