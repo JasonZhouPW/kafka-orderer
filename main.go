b1733c3e780f839ecb2a107c656ffb23e6b8086c
Kostas Christidis <kostas@christidis.io>
2016-08-10 15:02:02 -0400

    Turn off the verbose flag

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ import (
 
 var (
 	brokers = flag.String("brokers", "localhost:9092", "The Kafka brokers to connect to, as a comma-separated list")
-	role    = flag.String("role", "p", "The client type: [p]roducer, [c]consumer, [g]eneric client, [b]roker)")
+	role    = flag.String("role", "p", "The client type: [p]roducer, [c]consumer, [g]eneric client)")
 	topic   = flag.String("topic", "test", "The topic to publish/consume to/from")
-	verbose = flag.Bool("verbose", true, "Turn on logging for the sarama library")
+	verbose = flag.Bool("verbose", false, "Turn on logging for the sarama library")
 
 	sendDuration    = 15 * time.Second
 	startFromOffset = sarama.OffsetOldest
