23bb31e8cceea5a665419ad342635358a2572bdc
Kostas Christidis <kostas@christidis.io>
2016-08-10 21:19:45 -0400

    Change type for duration flag

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ import (
 )
 
 var (
-	begin    = flag.Int64("begin", -2, "Offset the consumer should begin from, if available on the broker: -2 = oldest available, -1 = newest (ignore the past), >0: pick precise offset")
-	brokers  = flag.String("brokers", "localhost:9092", "The Kafka brokers to connect to, as a comma-separated list")
-	duration = flag.Int("duration", 15, "Number of seconds for which the producer will send messages")
-	role     = flag.String("role", "p", "The client type: [p]roducer, [c]consumer, [g]eneric client)")
-	topic    = flag.String("topic", "test", "The topic to publish/consume to/from")
-	verbose  = flag.Bool("verbose", false, "Turn on logging for the sarama library")
+	begin    = flag.Int64("begin", -2, "Offset the consumer should begin from, if available on the broker: -2 = oldest available, -1 = newest (ignore the past), >0: pick precise offset - default: -2")
+	brokers  = flag.String("brokers", "localhost:9092", "The Kafka brokers to connect to, as a comma-separated list - default: localhost:9092")
+	duration = flag.Duration("duration", 15*time.Second, "Time during which the producer will send messages (at a rate of 1 msg/sec) - default: 15s")
+	role     = flag.String("role", "p", "The client type: [p]roducer, [c]consumer, [g]eneric client) - default: p")
+	topic    = flag.String("topic", "test", "The topic to publish/consume to/from - default: test")
+	verbose  = flag.Bool("verbose", false, "Turn on logging for the sarama library - default: false")
 
 	concurrentReqs = 1
 	requiredAcks   = sarama.WaitForAll
@@ -38,8 +38,7 @@ func main() {
 
 	switch *role {
 	case "p":
-		sendDuration := time.Duration(*duration) * time.Second
-		launchProducer(brokerList, config, *role, sendDuration)
+		launchProducer(brokerList, config, *role, *duration)
 	case "c":
 		startFromOffset := *begin
 		log.Printf("Chosen offset: %v\n", startFromOffset)
